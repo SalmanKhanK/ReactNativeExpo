c4e9785e1e0494ea72454866e89a95b29e25645e
Salman <m.salmank9991@gmail.com>
2021-11-22 21:28:15 -0800

    Button

--- App.js
+++ App.js
@@ -1,44 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text,TouchableHighlight,TouchableWithoutFeedback,TouchableOpacity,TouchableNativeFeedback, View,Image } from 'react-native';
+import { StyleSheet,View,Button ,Platform,Alert} from 'react-native';
 
 export default function App() {
+  const Separator = () => <View style={styles.separator} />;
   return (
     <View style={styles.container}>
-      <Text numberOfLines={1} onPress={()=>console.log("Hello world")}>Hello world good this is my first react naticve project which i working on it</Text>
-      {/* <TouchableHighlight activeOpacity={0.6} underlayColor="red" onPress={()=>console.log("Touch Image")}>
-      <Image source={{
-          width:200,
-          height:300,
-          uri:"https://picsum.photos/200/300"
-          }} />
-      </TouchableHighlight> */}
-
-      {/* <TouchableWithoutFeedback  onPress={()=>console.log("Touch Image")}>
-      <Image source={{
-          width:200,
-          height:300,
-          uri:"https://picsum.photos/200/300"
-          }} />
-      </TouchableWithoutFeedback> */}
-{/* 
-            <TouchableOpacity  onPress={()=>console.log("Touch Image")}>
-      <Image source={{
-          width:200,
-          height:300,
-          uri:"https://picsum.photos/200/300"
-          }} />
-      </TouchableOpacity> */}
-
-      
-<TouchableNativeFeedback  onPress={()=>console.log("Touch Image")}>
-      <Image source={{
-          width:200,
-          height:300,
-          uri:"https://picsum.photos/200/300"
-          }} />
-      </TouchableNativeFeedback>
-
-      
+     <Button title="Click me" color="#841584"    onPress={()=>console.log("Button tutorial ")} />
+     <Separator />
+     <Button title="Alert Button" color="#841584"    onPress={()=>alert("Button clicked")} />
+     <Separator />
+     <Button title="Alert Api" color="#841584"    onPress={()=>Alert.alert("Title","Message",[
+       {text: "Yes",onPress: ()=>console.log("Yes")},
+       {text: "No", onPress: ()=>console.log("No")}
+     ])} />
     </View>
   );
 }
@@ -51,4 +25,9 @@ const styles = StyleSheet.create({
     alignItems:"center",
     paddingTop: Platform.OS === 'android' ? 25 : 0
   },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  }
 });
