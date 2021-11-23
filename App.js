1ce850929e28c3ad304a4977fce26dc489bc0035
Salman <m.salmank9991@gmail.com>
2021-11-22 20:52:14 -0800

    images with touchAbles

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,TouchableHighlight,TouchableWithoutFeedback,TouchableOpacity,TouchableNativeFeedback, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text numberOfLines={1} onPress={()=>console.log("Hello world")}>Hello world good this is my first react naticve project which i working on it</Text>
+      {/* <TouchableHighlight activeOpacity={0.6} underlayColor="red" onPress={()=>console.log("Touch Image")}>
+      <Image source={{
+          width:200,
+          height:300,
+          uri:"https://picsum.photos/200/300"
+          }} />
+      </TouchableHighlight> */}
+
+      {/* <TouchableWithoutFeedback  onPress={()=>console.log("Touch Image")}>
+      <Image source={{
+          width:200,
+          height:300,
+          uri:"https://picsum.photos/200/300"
+          }} />
+      </TouchableWithoutFeedback> */}
+{/* 
+            <TouchableOpacity  onPress={()=>console.log("Touch Image")}>
+      <Image source={{
+          width:200,
+          height:300,
+          uri:"https://picsum.photos/200/300"
+          }} />
+      </TouchableOpacity> */}
+
+      
+<TouchableNativeFeedback  onPress={()=>console.log("Touch Image")}>
+      <Image source={{
+          width:200,
+          height:300,
+          uri:"https://picsum.photos/200/300"
+          }} />
+      </TouchableNativeFeedback>
+
+      
     </View>
   );
 }
@@ -15,7 +47,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"center",
+    alignItems:"center",
+    paddingTop: Platform.OS === 'android' ? 25 : 0
   },
 });
